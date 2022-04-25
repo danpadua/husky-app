cd0afd54209704b8321cb9fd3bdefdfcc51729da
Danilo de Pádua <danilo.padua@pilla.tech>
2022-04-25 16:15:59 -0300

    Finalizando configuração

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import 'teste.css';
 
 function App() {
-  let a = "Teste";
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let a = 'TEste';
+	let ad = 'TEste';
+	console.log('a', a);
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit
+					<code>src/App.js</code>
+					and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
